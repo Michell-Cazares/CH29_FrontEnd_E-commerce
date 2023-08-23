0559d0b87c9e8c286a9118dbaac6369072ad4630
MichellCazares <mc.michellcazares@gmail.com>
2023-08-22 20:07:57 -0600

    Se modificó el diagrama ER y las sentencias sql

--- MySQL/elotitosSentencias.sql
+++ MySQL/elotitosSentencias.sql
@@ -1,11 +1,12 @@
 use elotitos;
 
-INSERT INTO usuarios (correo, nombre, password, telefono, administrador) 
-VALUES ('itzelgps99@gmail.com', 'Itzel Guadalupe', 'Itzelgps99_', 7551308222, 1),
-('elotestoluca@gmail.com', 'Gina Molina', 'GinaM99_', 7221953594,1),
-('mc.michellcazares@gmail.com', 'Michell Cázares', 'Michell123_', 3121346641, 0),
-('susoto0916@gmail.com', 'Susana Soto', 'Susana09_', 5543029213,0),
-('pamela.geofisica@gmail.com', 'Brenda Juarez', 'Juroju_12', 5522974619,0);
+INSERT INTO usuarios (correo, nombre, password, telefono, userType) 
+VALUES ('itzelgps99@gmail.com', 'Itzel Guadalupe', 'Itzelgps99_', 7551308222, 'admin'),
+('elotestoluca@gmail.com', 'Gina Molina', 'GinaM99_', 7221953594,'admin');
+INSERT INTO usuarios (correo, nombre, password, telefono) 
+VALUES ('mc.michellcazares@gmail.com', 'Michell Cázares', 'Michell123_', 3121346641),
+('susoto0916@gmail.com', 'Susana Soto', 'Susana09_', 5543029213),
+('pamela.geofisica@gmail.com', 'Brenda Juarez', 'Juroju_12', 5522974619);
 
 INSERT INTO productos (nombre, descripcion, precio, imagen) 
 VALUES ('Elotes preparado', 'Elote preparado con mayonesa, queso, chile, limón y sal. Elote de grano pequeño (Tehuacán), por temporada.',25, 'https://michell-cazares.github.io/CH29_FrontEnd_E-commerce/src/img/fotosProductos/elotePreparadoTehuacan.png'),
@@ -19,14 +20,14 @@ VALUES ('Elotes preparado', 'Elote preparado con mayonesa, queso, chile, limón 
 ('DoriEsquites', 'Las papitas de su gusto, con esquite mayonesa, queso de nachos, queso blanco, sal, limón, y la salsa que guste.', 50, 'https://michell-cazares.github.io/CH29_FrontEnd_E-commerce/src/img/fotosProductos/doriEsquite.png' ), 
 ('Maruchan Esquites',' Las papitas de su gusto con Maruchan y con esquite, mayonesa, queso de nachos, queso blanco, sal, limón, y la salsa que guste', 70, 'https://michell-cazares.github.io/CH29_FrontEnd_E-commerce/src/img/fotosProductos/maruchanEsquites.png' );
 
-INSERT INTO compras (pago_total,fecha,cantidad_productos,idusuarios) 
+INSERT INTO compras (pago_total,fecha,cantidad_productos,idusuario) 
 VALUES (75,curdate(),3,1),
 (100,curdate(),2,3),
 (80,curdate(),2,5),
 (150,curdate(),3,4),
 (75,curdate(),3,1);
 
-INSERT INTO compras_detalle (idcompras, idproductos, cantidad_productos, venta_total)
+INSERT INTO compras_detalle (idcompra, idproducto, cantidad_productos, venta_total)
 VALUES(1,1,1,25),
 (1,3,2,50),
 (2,10,1,70),
@@ -38,3 +39,6 @@ VALUES(1,1,1,25),
 (4,8,1,30),
 (5,1,1,25),
 (5,3,2,50);
+
+select u.idusuario,u.correo,u.nombre,u.telefono,c.idcompra,c.pago_total as total , cd.cantidad_productos,p.nombre as nombre_producto
+from usuarios as u inner join compras as c on u.idusuario = c.idusuario inner join compras_detalle as cd on c.idcompra = cd.idcompra inner join productos as p on cd.idproducto = p.idproducto; 
